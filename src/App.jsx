2a6a03881c9ebe9352e4d2b1dff4c49a0b8468d4
edupoly <info.edupoly@gmail.com>
2022-12-06 07:17:15 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
-import Todolist from './Todolist';
 function App() {
   return (
     <div className="border border-2 border-danger p-2 m-2">
      <h1>Hello WOrld</h1>
-     <Counter></Counter>
-     <Todolist></Todolist>
     </div>
   );
 }
