b01ead03293a7235aca370feb7844f0bc406d1c2
edupoly <info.edupoly@gmail.com>
2022-12-08 08:25:22 +0530

    todolist done undo

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,7 +1,12 @@
 import React,{useState} from 'react'
 
 function Todolist() {
-  const [todos, setTodos] = useState(['clear junk','purchase fruits'])
+  const [todos, setTodos] = useState([
+                                        {title:'clear junk',status:false},
+                                        {title:'purchase fruits',status:false},
+                                        {title:'pens stand',status:true},
+                                        {title:'eat biryani',status:false}
+                                      ])
   const [newtodo, setNewtodo] = useState('')
   function handleChange(e){
     setNewtodo(e.target.value)
@@ -14,6 +19,16 @@ function Todolist() {
     temp.splice(index,1)
     setTodos([...temp])
   }
+  function undoTask(index){
+    var temp = [...todos];
+    temp[index].status = false
+    setTodos([...temp])
+  }
+  function doneTask(index){
+    var temp = [...todos];
+    temp[index].status = true
+    setTodos([...temp])
+  }
   return (
     <div className='border border-info border-3 p-2'>
       <h1>Todolist</h1>
@@ -22,8 +37,12 @@ function Todolist() {
       <ul>
         {
           todos.map((todo,i)=>{
-            return <li>
-              {todo}
+            return <li className={(todo.status===true)?'text-decoration-line-through':''}>
+              {todo.title}
+              {
+                todo.status===true
+                ?(<button onClick={()=>{undoTask(i)}}>Undo</button>)
+                :(<button onClick={()=>{doneTask(i)}}>Done</button>)}
               <button onClick={()=>{delTodo(i)}}>Del</button>
             </li>
           })
