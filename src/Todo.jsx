0215080d0479f81db64c132ac65cad962aa39ddc
edupoly <info.edupoly@gmail.com>
2022-12-09 08:21:13 +0530

    lifecycle

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { motion,AnimatePresence } from 'framer-motion'
+import React, { useEffect } from 'react'
+
 function Todo({todo,undoTask,doneTask,delTodo,i}) {
+  console.log(todo.title+" called")
+  useEffect(()=>{},[])
+  useEffect(()=>{})
+  useEffect(()=>{},todo.title)
   return (
-    <AnimatePresence>
-      <motion.li className='d-flex' key={i} initial={{x:-40}} animate={{ x: 4 }}
-      transition={{ ease: "easeOut", duration: 1 }} exit={{opacity:0,duration:2}}>
+
+      <li className='d-flex'>
         <div className={(todo.status===true)?'strike':''}>{todo.title}</div>
         <div>
           {
@@ -13,8 +16,7 @@ function Todo({todo,undoTask,doneTask,delTodo,i}) {
             :(<button onClick={()=>{doneTask(i)}}>Done</button>)}
           <button onClick={()=>{delTodo(i)}}>Del</button>
         </div>
-      </motion.li>
-    </AnimatePresence>
+      </li>
   )
 }
 
